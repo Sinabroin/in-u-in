895fbac8ef5866c10bbc08ef138a6e3de53231c9
Sinabroin <146015596+Sinabroin@users.noreply.github.com>
2024-11-24 01:16:58 +0900

    Update additional.css

--- styles/additional.css
+++ styles/additional.css
@@ -1,16 +1,7 @@
-.custom-scrollbar::-webkit-scrollbar {
-  width: 8px;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
 }
 
-.custom-scrollbar::-webkit-scrollbar-track {
-  background: #f1f1f1;
-}
-
-.custom-scrollbar::-webkit-scrollbar-thumb {
-  background: #888;
-  border-radius: 4px;
-}
-
-.custom-scrollbar::-webkit-scrollbar-thumb:hover {
-  background: #555;
-}
+module.exports = nextConfig
